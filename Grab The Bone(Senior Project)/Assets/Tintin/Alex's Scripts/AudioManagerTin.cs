3e486323c00df1b130be6065047794e3fe34d022
tintinjokes <43967697+tintinjokes@users.noreply.github.com>
2020-02-04 17:21:50 -0800

    Update AudioManagerTin.cs

--- Grab The Bone(Senior Project)/Assets/Tintin/Alex's Scripts/AudioManagerTin.cs
+++ Grab The Bone(Senior Project)/Assets/Tintin/Alex's Scripts/AudioManagerTin.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManagerTin : MonoBehaviour
 {
     public static AudioManager instance;
 
