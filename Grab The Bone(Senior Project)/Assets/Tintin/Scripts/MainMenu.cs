3357960ee770ff8b5497dee783057211a9114258
tintinjokes <43967697+tintinjokes@users.noreply.github.com>
2020-02-09 17:43:20 -0800

    Updated the Main Menu script.
    Should operate a lot cleaner. Both credits and options are working.

--- Grab The Bone(Senior Project)/Assets/Tintin/Scripts/MainMenu.cs
+++ Grab The Bone(Senior Project)/Assets/Tintin/Scripts/MainMenu.cs
@@ -11,45 +11,49 @@ public class MainMenu : MonoBehaviour
     public GameObject CreditsUI;
     public GameObject BackButtonUI;
     public GameObject OptionsButtonUI;
-
+    public GameObject StartButtonUI;
+    public GameObject CreditsText;
     // static bool isLoaded = false;
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (IsPaused)                              // I don't know if I don't want this yet
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
-    */
-    /* public void StartGame()
-     {
-         MainMenuUI.SetActive(false);
-         Time.timeScale = 1f;
-         IsPaused = false;
-     }
+                                                                                                                // Update is called once per frame
+                                                                                                                 //void Update()
+                                                                                                                //{
+                                                                                                                  //  if (Input.GetKeyDown(KeyCode.Escape))
+                                                                                                                  ///    if (IsPaused)                              // I don't know if I don't want this yet
+                                                                                                                     //   {
+                                                                                                           ///      Resume();
+                                                                                                                       // }
+                                                                                                                      //  else
+                                                                                                                       // {
+                                                                                                                        //    Pause();
+                                                                                                                       // }
+                                                                                                                  //  }
+                                                                                                                //}
+                                                                                                                
+                                                                                                               //  public void StartGame()
+                                                                                                                // {
+                                                                                                              //       MainMenuUI.SetActive(false);
+                                                                                                                  //   Time.timeScale = 1f;
+                                                                                                                    // IsPaused = false;
+                                                                                                                 //}
 
-     void Pause()
-     {
-         pauseMenuUI.SetActive(true);
-         Time.timeScale = 0f;
-         IsPaused = true;
-     }
-     */
+                                                                                                                // void Pause()
+                                                                                                                // {
+                                                                                                                 //    pauseMenuUI.SetActive(true);
+                                                                                                                 //    Time.timeScale = 0f;
+                                                                                                                 //    IsPaused = true;
+                                                                                                                // }
+                                                                                                                
     
 
 
     private void Start()
     {
-        
+        StartButtonUI.SetActive(true);
+        OptionsButtonUI.SetActive(true);
+        CreditsUI.SetActive(true);
+        BackButtonUI.SetActive(false);
+        CreditsText.SetActive(false);
     }
     private void Update()
     {
@@ -61,9 +65,30 @@ public class MainMenu : MonoBehaviour
         //SceneManager.LoadScene("");
     }
 
+    public void BackButton()
+    {
+        StartButtonUI.SetActive(true);
+        OptionsButtonUI.SetActive(true);
+        CreditsUI.SetActive(true);
+        BackButtonUI.SetActive(false);
+        CreditsText.SetActive(false);
+    }
+
+    public void CreditsButton()
+    {
+        StartButtonUI.SetActive(false);
+        OptionsButtonUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        BackButtonUI.SetActive(true);
+        CreditsText.SetActive(true);
+    }
     public void Options()
     {
-
+        StartButtonUI.SetActive(false);
+        OptionsButtonUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        BackButtonUI.SetActive(true);
+        CreditsText.SetActive(false);
     }
     public void QuitGame()
     {
