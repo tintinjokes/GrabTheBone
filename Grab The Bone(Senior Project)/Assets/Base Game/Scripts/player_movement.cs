28bdd2a5b493ec69391e10f95c4c2c6acf0fdf91
CorreianAngel <43967709+CorreianAngel@users.noreply.github.com>
2020-02-23 13:53:04 -0800

    Modified level
    The dog can run and jump normally and the level has been slightly modified.

--- Grab The Bone(Senior Project)/Assets/Base Game/Scripts/player_movement.cs
+++ Grab The Bone(Senior Project)/Assets/Base Game/Scripts/player_movement.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 
 public class player_movement : MonoBehaviour
 {
     Rigidbody2D rb;
+    public Animator animator;
 
     public float speed;
     public float jumpforce;
     public bool jump = true;
     public float speedboost;
-
+    private TextMeshProUGUI scoreCounterText;
     public int score = 0;
-
+    //public static AudioManager instance;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreCounterText = GetComponent<TextMeshProUGUI>();
         Debug.Log(score);
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -30,7 +35,11 @@ public class player_movement : MonoBehaviour
         if(Input.GetButtonDown("Fire1") && jump == true)
         {
             jumping();
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsRunning", false);
+            
         }
+
     }
 
     void jumping()
@@ -44,6 +53,9 @@ public class player_movement : MonoBehaviour
         if(collision.gameObject.tag.Equals("Ground"))
         {
             jump = true;
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsRunning", true);
+
         }
 
 
