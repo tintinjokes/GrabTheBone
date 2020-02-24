e5a8f0f650cb5b93f79e94cb482ce781a3f74ee4
tintinjokes <43967697+tintinjokes@users.noreply.github.com>
2020-02-24 13:12:45 -0800

    Made a shooting mechanic because yeah

---
+++ Grab The Bone(Senior Project)/Assets/Tintin/Scripts/Shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject tennisball;
+    public Transform shootpoint;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) { 
+            Fire();
+            Debug.Log("HEY YOU PRESSED THE FIRE BUTTON");
+        }
+    }
+
+    void Fire()
+    {
+        Instantiate(tennisball, shootpoint);
+    }
+}
---
+++ Grab The Bone(Senior Project)/Assets/Tintin/Scripts/TennisBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TennisBall : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
