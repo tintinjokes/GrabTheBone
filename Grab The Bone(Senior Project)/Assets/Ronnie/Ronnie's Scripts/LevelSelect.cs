0ba5eb9b53417897bf2108f171af7f3331b7f589
Ronnie James <ronniejespinosa1998@gmail.com>
2020-04-09 16:12:33 -0700

    Menu Changes
    Changes to Main Menu
    - Links to Level Select
    - Added Level Select
    -- Level Select connects to Level 01 and Level 02
    - Pause Menu Quit now Main Menu
    - Fixed sprites in Main Menu

---
+++ Grab The Bone(Senior Project)/Assets/Ronnie/Ronnie's Scripts/LevelSelect.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
