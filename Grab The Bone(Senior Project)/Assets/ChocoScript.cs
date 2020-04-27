503d938d9fa90ea813aa38cc8a300b30634aa619
tintinjokes <43967697+tintinjokes@users.noreply.github.com>
2020-04-27 15:09:02 -0700

    Choco Script and Collectible Script fixed

--- Grab The Bone(Senior Project)/Assets/ChocoScript.cs
+++ Grab The Bone(Senior Project)/Assets/ChocoScript.cs
@@ -1,20 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class collectable_Script : MonoBehaviour
+public class ChocoScript : MonoBehaviour
 {
-
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
 
+    // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "TennisBall")
         {
             Destroy(gameObject);
         }
