dfa1e200c16bb5358c50c6b7c9ce331061c09781
fritoman95 <robert.freitas95@gmail.com>
2020-04-30 14:01:15 -0700

    changes
    yus

---
+++ Grab The Bone(Senior Project)/Assets/Robert's Files/Scripts/MenuChanges.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MenuChanges : MonoBehaviour
+{
+    public GameObject mainMenuCanvas;
+    public GameObject levelCanvas;
+    public GameObject optionsCanvas;
+    public GameObject creditsCanvas;
+
+
+    private void Start()
+    {
+        mainMenuCanvas.SetActive(true);
+        levelCanvas.SetActive(false);
+        optionsCanvas.SetActive(false);
+        creditsCanvas.SetActive(false);
+    }
+
+
+
+    public void PressStart()
+    {
+        levelCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+    }
+
+
+
+    public void PressOptions()
+    {
+        optionsCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+    }
+
+
+
+    public void PressCredits()
+    {
+        creditsCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+    }
+
+
+    public void PressBack()
+    {
+        if(levelCanvas)
+        {
+            mainMenuCanvas.SetActive(true);
+            levelCanvas.SetActive(false);
+        }
+
+        if(optionsCanvas)
+        {
+            mainMenuCanvas.SetActive(true);
+            optionsCanvas.SetActive(false);
+        }
+
+        if(creditsCanvas)
+        {
+            mainMenuCanvas.SetActive(true);
+            creditsCanvas.SetActive(false);
+        }
+    }
+
+    public void LevelSelect(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
+
---
+++ Grab The Bone(Senior Project)/Assets/Robert's Files/Scripts/Musicadjust.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Musicadjust : MonoBehaviour
+{
+
+    // Reference to Audio Source component
+    private AudioSource audioSrc;
+
+    // Music volume variable that will be modified
+    // by dragging slider knob
+    
+    public float musicVolume = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Assign Audio Source component to control it
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Setting volume option of Audio Source to be equal to musicVolume
+        audioSrc.volume = musicVolume;
+    }
+
+    // Method that is called by slider game object
+    // This method takes vol value passed by slider
+    // and sets it as musicValue
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
