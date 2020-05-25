3b9e199313cb5b86e3b401d563a376160c8d7fe6
CorreianAngel <43967709+CorreianAngel@users.noreply.github.com>
2020-05-25 08:12:24 -0700

    Fixed screen for build in level02

--- Grab The Bone(Senior Project)/Assets/Alex's Files/Alex's Scripts/GameManager.cs
+++ Grab The Bone(Senior Project)/Assets/Alex's Files/Alex's Scripts/GameManager.cs
@@ -29,6 +29,7 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
         Time.timeScale = 1f;
         Score_Script.scorecount = 0;
+        ballCounter.ballcount = 0;
     }
 
     public void Restart() 
@@ -36,6 +37,7 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1f;
         Score_Script.scorecount = 0;
+        ballCounter.ballcount = 0;
     }
 
 
